a0b5d40e7e022fe29bf0b8eb50216466e2152fe4
Anuj <anujsde@gmail.com>
2024-05-17 21:36:56 +0530

    complain history

--- src/component/HelpSupport.js
+++ src/component/HelpSupport.js
@@ -1,15 +1,19 @@
-import React, { useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useContext, useEffect, useState } from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
 import { Context } from '../Context/Context';
 import { Support } from '../Api/Support';
 import { Flip, toast } from 'react-toastify';
+import { Complain } from '../Api/Complain';
 
 export default function HelpSupport() {
 
     const navigate = useNavigate();
     const [subject, setSubject] = useState("");
     const [message, setMessage] = useState("");
-    const {setLoad,setLoadColor}= useContext(Context)
+    const { setLoad, setLoadColor } = useContext(Context)
+    const [complainList, setComplainList ] = useState([])
+
+    const {pathname} = useLocation();
 
     const handleSubmit = async () => {
         setLoad(true);
@@ -56,12 +60,49 @@ export default function HelpSupport() {
         setLoad(false);
     }
 
+    useEffect(() => {
+        if (pathname === '/helpSupport') {
+            handleComplain();
+        }
+    }, [pathname])
+
+    const handleComplain = async () => {
+        setLoad(true);
+        setLoadColor("#434343");
+        let data = {
+            "username": JSON.parse(localStorage.getItem('userinfo'))?.username
+        }
+        let res = await Complain(data);
+        if (res) {
+            if (res.data.status) {
+                setComplainList(res.data.data)
+            }
+            else {
+                toast.error(res.data.message, {
+                    position: "top-right",
+                    autoClose: 1500,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                    transition: Flip,
+                });
+            }
+            setLoad(false);
+        }
+        setLoad(false);
+    }
+
+    console.log(complainList);
+
     return (
         <>
             <div className='depositContainer'>
                 <div className="wrapper" style={{ marginTop: "0px" }}>
                     <div className="section">
-                        <div className="top_navbar" style={{ height: "60px" }}>
+                        <div className="top_navbar" style={{ height: "60px",zIndex:"111" }}>
                             <div className="hamburger" >
                                 <a className='back_btn'>
                                     <i className="fa-solid fa-chevron-left" onClick={() => navigate("/game")}></i>
@@ -73,7 +114,7 @@ export default function HelpSupport() {
                     </div>
                 </div>
 
-                <div class="container" style={{ position: "relative", top: "115px" }}>
+                <div class="container" style={{ position: "relative", top: "75px" }}>
                     <h2>Send Message</h2>
                     <div>
                         <div class="form-group">
@@ -86,6 +127,37 @@ export default function HelpSupport() {
                         </div>
                         <button type="submit" class="btn-submit" onClick={handleSubmit}>Send</button>
                     </div>
+                </div>
+
+                <div className="Tablecontainer" style={{ marginTop: "150px", width: "100%" }}>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th>Ticket ID</th>
+                                <th>Subject</th>
+                                <th>Message</th>
+                                <th>Time</th>
+                                <th>Status</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {complainList?.map((ele, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td >{ele?._id.slice(0,5)}</td>
+                                        <td>{ele?.subject}</td>
+                                        <td>{ele?.message}</td>
+                                        <td>{new Date(ele?.createdAt).toUTCString()}</td>
+                                        <td>
+                                            {ele?.status === 0 && <button type="button" class="btn btn-warning btnyellow-custom">Pending</button>}
+                                            {ele?.status === 1 && <button type="button" class="btn btn-success btngreen-success-custom">Success</button>}
+                                            {ele?.status === 2 && <button type="button" class="btn btn-danger btnred-danger-custom">Rejected</button>}
+                                        </td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
                 </div>
             </div >
         </>
